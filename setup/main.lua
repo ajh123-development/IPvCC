cabe47f803cbcdf46fc49706c8d2995db40f9ea3
ajh123 <ajh123@users.noreply.github.com>
2022-08-21 16:53:19 +0100

    Installer files 4

--- setup/main.lua
+++ setup/main.lua
@@ -1,4 +1,4 @@
-fs.delete("tmp/IPvCC/")
+fs.delete("tmp/IPvCC")
 print("Packaging directories...")
 fs.delete("IPvCC_programs")
 fs.makeDir("IPvCC_programs")
@@ -7,7 +7,11 @@ fs.move("IPvCC/ifconfig.lua", "IPvCC_programs/ifconfig.lua")
 fs.move("IPvCC/testSend.lua", "IPvCC_programs/testSend.lua")
 fs.move("IPvCC/testRecv.lua", "IPvCC_programs/testRecv.lua")
 
-fs.move("IPvCC/IPvCC", "IPvCC")
 fs.delete("IPvCC/setup")
+fs.move("IPvCC/IPvCC", "tmp/IPvCC")
+fs.delete("IPvCC")
+fs.makeDir("IPvCC")
+fs.move("tmp/IPvCC", "IPvCC")
+fs.delete("tmp/IPvCC")
 
 print("Finished installing IPvCC")
